74dcf5c04c41cbe9fac319a13a09d123ab0f5f67
Dayanithi Hari <dayanithihari@Dayanithis-MacBook-Pro.local>
2016-06-04 17:00:21 +0100

    Mongo client connect to get movies collection- completed week1

--- app.js
+++ app.js
@@ -3,27 +3,49 @@
  * Initializes the application and starts the server.
  */
 
-var express = require('express');
-    cons=require('consolidate');
-    mongodb=require('mongodb');
-    app = express();
+ var express = require('express');
+ cons=require('consolidate');
+ MongoClient = require('mongodb').MongoClient;
+ assert=require('assert');
+ app = express();
 
 
-app.engine('html',cons.nunjucks);
-app.set('view engine','html');
-app.set('views',__dirname+'/views');
+ app.engine('html',cons.nunjucks);
+ app.set('view engine','html');
+ app.set('views',__dirname+'/views');
 
-app.get('/',function(req,res){
-	res.render('hello',{'name':'Templates'});
+
+
+ /* Mongo db connection*/
+ var URL = 'mongodb://localhost:27017/videos';
+
+
+
+ MongoClient.connect(URL, function(err, db) {
+
+ 	assert.equal(null,err);
+ 	console.log("successfully connected  to MongoDB.");
+ 	var collection = db.collection('movies');
+
+
+ 	app.get('/',function(req,res){
+ 		collection.find({}).toArray(function(err, docs) {
+ 			res.render('movies',{'movies':docs});
+ 			db.close()
+ 		});
+ 	});
+ 	app.use(function(req,res)
+ 	{
+ 		res.sendStatus(404);
+ 	});
+
+	//starts the server
+	var port = process.env.PORT || 3000;
+	app.listen(port, function() {
+		console.log('Express server listening on port : ' + port);
+	});
+
 });
 
-app.use(function(req,res)
-{
- res.sendStatus(404);
-});
 
-//starts the server
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-	console.log('Express server listening on port : ' + port);
-});
+
